75e4300b4110503ed8b0fa28c2a640879750bc91
Lamboll <rlamboll@ic.ac.uk>
2019-11-27 14:18:05 +0000

    established data extraction for the FaIR model

--- src/distributions_of_inputs.py
+++ src/distributions_of_inputs.py
@@ -1,14 +1,78 @@
+import os
+
+import netCDF4
 import numpy as np
 import pandas as pd
+
 
 def tcre_distribution(mean, sd, n_return=1):
     return np.random.normal(mean, sd, n_return)
 
 
-def establish_temp_dependence(file, temps):
-    non_co2_col = "non-co2 warming (rel. to 2010-2019) at peak cumulative emissions co2 (rel. to 2015-2015)"
-        #"non-co2 warming (rel. to 2010-2019) at peak surface temperature (rel. to 2010-2019)"
-    temp_col = "peak surface temperature (rel. to 2010-2019)"
-    db = pd.read_csv(file)
+def establish_temp_dependence(db, temps, non_co2_col, temp_col):
     regres = np.polyfit(db[temp_col], db[non_co2_col], 1)
     return pd.Series(index=temps, data=temps * regres[0] + regres[1])
+
+
+def load_data_from_MAGICC(
+        file,
+        non_co2_col,
+        temp_col,
+        model_col,
+        scenario_col,
+        year_col,
+):
+    df = pd.read_csv(file)
+    df = df.loc[df["quantile"] == 0.5]
+    df = df[[non_co2_col, temp_col, model_col, scenario_col, year_col]]
+    assert len(df) == len(df.groupby([model_col, scenario_col]))
+    return df
+
+
+def load_data_from_FaIR(
+        folder_all,
+        folder_co2_only,
+        desired_scenarios_db,
+        model_col,
+        scenario_col,
+        year_col,
+):
+    all_files = os.listdir(folder_all)
+    CO2_only_files = os.listdir(folder_co2_only)
+    assert all_files == CO2_only_files
+    # We must find the correspondence between the file systems and the known years of
+    # peak emissions
+    expected_filenames = "IPCCSR15_" + desired_scenarios_db[model_col] + "_" + \
+                         desired_scenarios_db[scenario_col]
+    assert len(expected_filenames) == len(set(expected_filenames)), \
+        "Expected file names are not clearly distinguishable"
+    expected_years = desired_scenarios_db[year_col]
+    compare_filenames = [x.replace(" ", "_").replace(".", "_")[:-12] for x in all_files]
+    assert len(compare_filenames) == len(set(compare_filenames)), \
+        "Processed file names are not clearly distinguishable"
+    files_found_indx = [(y in compare_filenames) for y in expected_filenames]
+    expected_filenames = expected_filenames[files_found_indx]
+    expected_years = expected_years[files_found_indx]
+    temp_all_dbs = []
+    temp_only_co2_dbs = []
+    for ind in range(len(expected_filenames)):
+        file = [i for (i, v) in zip(all_files, [
+            expected_filenames.iloc[ind] == y for y in compare_filenames
+        ]) if v]
+        assert len(file) == 1
+        file = file[0]
+        open_link_all = netCDF4.Dataset(folder_all + file)
+        time_ind = np.where(open_link_all.variables["time"] == expected_years.iloc[ind])
+        assert len(time_ind) == 1, "There must be exactly one match between the years" \
+                                   "and the expected year, but there are {}" \
+            .format(sum(time_ind))
+        time_ind = time_ind[0]
+        temp_all_dbs.append(
+            pd.DataFrame(open_link_all["temp"][time_ind, ::1]).mean(axis=1)
+        )
+        open_link_co2_only = netCDF4.Dataset(folder_co2_only + file)
+        temp_only_co2_dbs.append(
+            pd.DataFrame(open_link_co2_only["temp"][time_ind, ::1]).mean(axis=1))
+        # TODO: calculate offsets
+    dbs = pd.DataFrame({"temp_all": temp_all_dbs, "temp_CO2": temp_only_co2_dbs}, dtype="float32")
+    return dbs
--- src/run_budget_calcuator.py
+++ src/run_budget_calcuator.py
@@ -6,6 +6,7 @@ import src.budget_calculator_functions as budget_func
 
 # Input values
 # ______________________________________________________________________________________
+
 # The target temperature changes to achieve. (Units: C)
 dT_targets = np.arange(1.1, 2.2, 0.1)
 # The number of loops performed for each temperature
@@ -15,8 +16,6 @@ n_loops = 1000000
 zec = 0.0
 # The temperature difference already seen. (Units: C)
 historical_dT = 1.0
-# The non-carbon contributions to temperature change. (Units: C)
-non_co2_dT_file = "../InputData/AR6comptibleMAGICCsetup.csv"
 # The mean of the distribution of TCRE. We use units of C per GtCO2.
 # (TCRE = Transient climate response to cumulative carbon emissions)
 tcre_mean = (0.2 + 0.7) / 2000
@@ -30,6 +29,25 @@ quantiles_to_report = np.array([0.33, 0.5, 0.66])
 # Output file location.
 output_file = "../Output/budget_calculation.csv"
 
+#   Information for reading in files:
+#   MAGICC files
+# The file in which we find the MAGICC model estimate for the non-carbon contributions
+# to temperature change. (Units: C)
+non_co2_magicc_file = "../InputData/AR6comptibleMAGICCsetup.csv"
+# The names of the columns of interest in the MAGICC model file
+magicc_non_co2_col = "non-co2 warming (rel. to 2010-2019) at peak cumulative emissions co2 (rel. to 2015-2015)"
+# The name of the column containing the surface temperature of interest
+magicc_temp_col = "peak surface temperature (rel. to 2010-2019)"
+#
+model_col = "model"
+scenario_col = "scenario"
+year_col = "peak cumulative emissions co2 (rel. to 2015-2015) year"
+
+#   FaIR files
+# The file for the unscaled anthropological temperature changes
+fair_anthro_folder = "../InputData/fair141_sr15_ar6fodsetup/FAIR141anthro_unscaled/"
+fair_co2_only_folder = "../InputData/fair141_sr15_ar6fodsetup/FAIR141CO2_unscaled/"
+
 # ______________________________________________________________________________________
 # The parts below should not need editing
 
@@ -37,9 +55,26 @@ budget_quantiles = pd.DataFrame(index=dT_targets, columns=quantiles_to_report)
 budget_quantiles.index.name = "dT_targets"
 # We interpret the higher quantiles as meaning a smaller budget
 quantiles_to_report = 1 - quantiles_to_report
-non_co2_dT_interp = distributions.establish_temp_dependence(non_co2_dT_file, dT_targets-historical_dT)
+magicc_db = distributions.load_data_from_MAGICC(
+    non_co2_magicc_file,
+    magicc_non_co2_col,
+    magicc_temp_col,
+    model_col,
+    scenario_col,
+    year_col,
+)
+non_co2_dT_magicc = distributions.establish_temp_dependence(
+    magicc_db,
+    dT_targets-historical_dT,
+    magicc_non_co2_col,
+    magicc_temp_col,
+)
+non_co2_dT_fair = distributions.load_data_from_FaIR(fair_anthro_folder, fair_co2_only_folder, magicc_db, model_col,
+    scenario_col,
+    year_col,)
+# TODO: include FaIR non-CO2 contributions.
 for dT_target in dT_targets:
-    non_co2_dT = non_co2_dT_interp.loc[dT_target]
+    non_co2_dT = non_co2_dT_magicc.loc[dT_target - historical_dT]
     tcres = distributions.tcre_distribution(tcre_mean, tcre_sd, n_loops)
     budgets = budget_func.calculate_budget(
         dT_target, zec, historical_dT, non_co2_dT, tcres, earth_feedback_co2
